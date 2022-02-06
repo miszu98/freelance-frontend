9579de1dc173b13b33a0ca1f784d4d30a6e4a5c1
Michal Malek <michal.malek98@wp.pl>
2022-02-06 13:28:31 +0100

    added switching size of elements in carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
+import { CarouselComponent } from 'angular-responsive-carousel';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,41 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Freelance';
 
+
+  @ViewChild('carousel') declare carousel: CarouselComponent;
+
   popularServicesImages = [
-    {path: '../assets/images/logo-design-2x.jpg', text: 'testowy'},
+    {path: '../assets/images/logo-design-2x.jpg'},
     {path: '../assets/images/social-2x.jpg'},
     {path: '../assets/images/voiceover-2x.jpg'},
     {path: '../assets/images/wordpress-2x.jpg'},
     {path: '../assets/images/animated-explainer-2x.jpg'}
   ];
+
+  ngOnInit() {
+    
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindoResize(event: any) {
+     
+    if (window.innerWidth <= 1120) {
+      this.carousel.cellsToShow = 2;
+      return;
+    }
+    if (window.innerWidth <= 1580) {
+      this.carousel.cellsToShow = 3;
+      return;
+    } 
+    if (window.innerWidth > 1580) {
+      this.carousel.cellsToShow = 5;
+      return;
+    } 
+
+    console.log(window.innerWidth);
+ 
+
+    
+  }
 
 }
