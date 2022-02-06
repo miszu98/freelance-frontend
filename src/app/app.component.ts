d1196c77f4eb9203be369bbe595374818f9e8be5
Michal Malek <michal.malek98@wp.pl>
2022-02-06 12:40:38 +0100

    modified popular service carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Freelance';
+
+  popularServicesImages = [
+    {path: '../assets/images/logo-design-2x.jpg', text: 'testowy'},
+    {path: '../assets/images/social-2x.jpg'},
+    {path: '../assets/images/voiceover-2x.jpg'},
+    {path: '../assets/images/wordpress-2x.jpg'},
+    {path: '../assets/images/animated-explainer-2x.jpg'}
+  ];
+
 }
