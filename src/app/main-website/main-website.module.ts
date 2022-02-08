2e316191c37e440f8d7413fa1442c910d8aad1f5
Michal Malek <michal.malek98@wp.pl>
2022-02-08 22:33:37 +0100

    created new module - main website

---
+++ src/app/main-website/main-website.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainWebsiteComponent } from './main-website.component';
+
+
+
+@NgModule({
+  declarations: [
+    MainWebsiteComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MainWebsiteModule { }
